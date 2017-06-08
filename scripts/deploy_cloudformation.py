e0263e59ffbf961ff6715fd2e3f70b00367920a6
Stephen Poole <stephen@melon.software>
2017-06-08 15:53:19 +0100

    Update deployment scripts

--- scripts/deploy_cloudformation.py
+++ scripts/deploy_cloudformation.py
@@ -1,6 +1,5 @@
+#!/usr/bin/env python
 # Upload a cloudformation template to S3, then run a stack update
-import pickle
-from collections import namedtuple
 
 import boto3
 import argparse
@@ -12,61 +11,35 @@ template_s3_bucket = 'biometrix-preprocessing-infrastructure'
 template_s3_path = 'cloudformation/'
 
 
-SessionToken = namedtuple('SessionToken', ['access_key_id', 'secret_access_key', 'session_token'])
-
-
-def get_sts_session_token(iam_user, mfa_token) -> SessionToken:
-    if os.path.isfile('.awstoken.p'):
-        print('Unpickling session token')
-        session_token = pickle.load(open(".awstoken.p", "rb"))
-        return session_token
-
-    sts_client = boto3.client('sts', region_name=aws_region)
-    session_token_data = sts_client.get_session_token(
-        SerialNumber='arn:aws:iam::532972427115:mfa/{}'.format(iam_user),
-        TokenCode=mfa_token
-    )['Credentials']
-
-    session_token = SessionToken(
-        access_key_id=session_token_data['AccessKeyId'],
-        secret_access_key=session_token_data['SecretAccessKey'],
-        session_token=session_token_data['SessionToken']
-    )
-
-    print('Pickling session token')
-    pickle.dump(session_token, open(".awstoken.p", "wb"))
-
-    return session_token
-
-
 def get_boto3_resource(resource):
     return boto3.resource(
         resource,
         region_name=aws_region,
-        aws_access_key_id=session_token.access_key_id,
-        aws_secret_access_key=session_token.secret_access_key,
-        aws_session_token=session_token.session_token
+        # aws_access_key_id=session_token.access_key_id,
+        # aws_secret_access_key=session_token.secret_access_key,
+        # aws_session_token=session_token.session_token
     )
 
 
 def upload_cf_stack(template):
     print('Uploading stack')
     s3_resource = get_boto3_resource('s3')
-    data = open("{}/{}".format(template_local_dir, template), 'rb')
-    s3_resource.Bucket(template_s3_bucket).put_object(Key=template_s3_path + template, Body=data)
+    data = open(template, 'rb')
+    s3_path = template_s3_path + os.path.basename(template)
+    s3_resource.Bucket(template_s3_bucket).put_object(Key=s3_path, Body=data)
+    return s3_path
 
 
-def update_cf_stack(stack_name, template):
+def update_cf_stack(stack_name, uploaded_path):
     print('Updating CloudFormation stack')
     cf_resource = get_boto3_resource('cloudformation')
     stack = cf_resource.Stack(stack_name)
 
     stack.update(
-        TemplateURL='https://s3.amazonaws.com/{bucket}/{path}{template}'.format(
+        TemplateURL='https://s3.amazonaws.com/{bucket}/{template}'.format(
             region=aws_region,
             bucket=template_s3_bucket,
-            path=template_s3_path,
-            template=template
+            template=uploaded_path,
         ),
         Parameters=[{'ParameterKey': p['ParameterKey'], 'UsePreviousValue': True} for p in stack.parameters or {}],
         Capabilities=['CAPABILITY_NAMED_IAM'],
@@ -83,17 +56,15 @@ if __name__ == '__main__':
                         nargs='?',
                         default='',
                         help='the name of a CF stack')
-    parser.add_argument('--mfatoken', '-t',
-                        type=str,
-                        default='',
-                        help='MFA token value')
+    # parser.add_argument('--mfatoken', '-t',
+    #                     type=str,
+    #                     default='',
+    #                     help='MFA token value')
 
     args = parser.parse_args()
 
-    session_token = get_sts_session_token('stephen.poole', args.mfatoken)
-
-    upload_cf_stack(args.template)
+    uploaded_path = upload_cf_stack(args.template)
 
     if args.stack != '':
-        update_cf_stack(args.stack, args.template)
+        update_cf_stack(args.stack, uploaded_path)
 
--- scripts/deploy_lambda.py
+++ scripts/deploy_lambda.py
@@ -1,69 +1,51 @@
 #!/usr/bin/env python
-# Upload a cloudformation template to S3, then run a stack update
+# Zip and upload a Lambda bundle
+import zipfile
 
 import boto3
 import argparse
 import os
 
 aws_region = 'us-east-1'
-template_local_dir = os.path.abspath('../cloudformation')
 template_s3_bucket = 'biometrix-preprocessing-infrastructure'
-template_s3_path = 'cloudformation/'
+template_s3_path = 'lambdas/'
 
 
 def get_boto3_resource(resource):
     return boto3.resource(
         resource,
         region_name=aws_region,
-        # aws_access_key_id=session_token.access_key_id,
-        # aws_secret_access_key=session_token.secret_access_key,
-        # aws_session_token=session_token.session_token
     )
 
 
-def upload_cf_stack(template):
-    print('Uploading stack')
+def upload_bundle(bundle, bucket):
+    print('Uploading bundle')
     s3_resource = get_boto3_resource('s3')
-    data = open(template, 'rb')
-    s3_resource.Bucket(template_s3_bucket).put_object(Key=template_s3_path + template, Body=data)
+    data = open(bundle, 'rb')
+    s3_resource.Bucket(bucket).put_object(Key=template_s3_path + os.path.basename(bundle), Body=data)
 
 
-def update_cf_stack(stack_name, template):
-    print('Updating CloudFormation stack')
-    cf_resource = get_boto3_resource('cloudformation')
-    stack = cf_resource.Stack(stack_name)
-
-    stack.update(
-        TemplateURL='https://s3.amazonaws.com/{bucket}/{path}{template}'.format(
-            region=aws_region,
-            bucket=template_s3_bucket,
-            path=template_s3_path,
-            template=template
-        ),
-        Parameters=[{'ParameterKey': p['ParameterKey'], 'UsePreviousValue': True} for p in stack.parameters or {}],
-        Capabilities=['CAPABILITY_NAMED_IAM'],
-    )
+def zip_bundle(filename):
+    print('Zipping bundle')
+    zip_filename = filename.replace('.py', '.zip')
+    zipf = zipfile.ZipFile(zip_filename, 'w', zipfile.ZIP_DEFLATED)
+    zipf.write(filename)
+    zipf.close()
+    return zip_filename
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Upload a template to S3, and maybe update a CF stack using it')
-    parser.add_argument('template',
+    parser = argparse.ArgumentParser(description='Upload a Lambda bundle to S3')
+    parser.add_argument('bundle',
                         type=str,
-                        help='the name of a template file')
-    parser.add_argument('stack',
+                        help='the name of a Lambda python file')
+    parser.add_argument('--bucket', '-b',
                         type=str,
-                        nargs='?',
-                        default='',
-                        help='the name of a CF stack')
-    # parser.add_argument('--mfatoken', '-t',
-    #                     type=str,
-    #                     default='',
-    #                     help='MFA token value')
+                        default=template_s3_bucket,
+                        help='S3 Bucket')
 
     args = parser.parse_args()
 
-    upload_cf_stack(args.template)
-
-    if args.stack != '':
-        update_cf_stack(args.stack, args.template)
+    zip_filename = zip_bundle(args.bundle)
+    upload_bundle(zip_filename, bucket=args.bucket)
 
