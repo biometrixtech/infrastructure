2ef2f443c33c13c1935675325df8d9100e5a58b1
Stephen Poole <stephen@melon.software>
2018-02-15 20:19:27 +0000

    Update bulk_invoke.py to work with new ingest dynamodb table

--- scripts/bulk_invoke.py
+++ scripts/bulk_invoke.py
@@ -3,6 +3,7 @@ import argparse
 import boto3
 import datetime
 import time
+from boto3.dynamodb.conditions import Attr
 
 
 def invoke_lambda(s3_bucket, s3_key, timestamp):
@@ -15,18 +16,42 @@ def invoke_lambda(s3_bucket, s3_key, timestamp):
 
 
 def process_file(s3_bucket, basename):
-    for s3_file, last_modified in list_s3_files(s3_bucket, basename):
-        invoke_lambda(s3_bucket, s3_file, last_modified.isoformat().rsplit('+', 1)[0])
+    # Reset the status flag in dynamodb to UPLOAD_IN_PROGRESS
+    ddb_session_events_table = dynamodb_resource.Table('preprocessing-{}-ingest-sessions'.format(args.environment))
+    print('    Setting sessionStatus to UPLOAD_IN_PROGRESS')
+    ddb_session_events_table.update_item(
+        Key={'id': basename},
+        UpdateExpression='SET sessionStatus = :sessionStatus, updatedDate = :updatedDate',
+        ExpressionAttributeValues={
+            ':sessionStatus': 'UPLOAD_IN_PROGRESS',
+            ':updatedDate': datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+        },
+    )
+
+    if args.reimport:
+        # Simulate a CloudWatch Event as if the file was newly-uploaded.  But keep the same 'uploaded at'
+        # timestamp to avoid duplicate records
+        for s3_file, last_modified in list_s3_files(s3_bucket, basename):
+            invoke_lambda(s3_bucket, s3_file, last_modified.strftime("%Y-%m-%dT%H:%M:%SZ"))
+    else:
+        # Just set the status flag to completed again
+        print('    Setting sessionStatus to UPLOAD_COMPLETE')
+        ddb_session_events_table.update_item(
+            Key={'id': basename},
+            ConditionExpression=Attr('id').exists(),
+            UpdateExpression='SET sessionStatus = :sessionStatus',
+            ExpressionAttributeValues={':sessionStatus': 'UPLOAD_COMPLETE'},
+        )
 
 
 def list_s3_files(s3_bucket, prefix, marker=''):
     ret = []
-    resp = s3_client.list_objects(Bucket=bucket, Prefix=prefix, Marker=marker)
+    resp = s3_client.list_objects(Bucket=s3_bucket, Prefix=prefix, Marker=marker)
     if 'Contents' not in resp:
         raise Exception('File {} not present in S3')
     ret.extend([(x['Key'], x['LastModified']) for x in resp['Contents'] if x['Key'][-8:] != 'combined'])
     if resp['IsTruncated']:
-        ret.extend(list_s3_files(bucket, prefix, ret[-1]))
+        ret.extend(list_s3_files(s3_bucket, prefix, ret[-1]))
     return sorted(ret)
 
 
@@ -48,9 +73,14 @@ if __name__ == '__main__':
                         type=str,
                         help='S3 bucket',
                         default='biometrix-preprocessing-{environment}-{region}')
+    parser.add_argument('--reimport',
+                        action='store_true',
+                        dest='reimport',
+                        help='Import all parts individually')
 
     args = parser.parse_args()
 
+    dynamodb_resource = boto3.resource('dynamodb', region_name=args.region)
     lambda_client = boto3.client('lambda', region_name=args.region)
     s3_client = boto3.client('s3')
 
