a973984667904a1f7ee1ddfb13512b0155498be2
Stephen Poole <stephen@melon.software>
2018-04-13 18:06:45 +0100

    Big refactor of deploy_cloudformation.py to deploy named versions of templates

--- scripts/deploy_cloudformation.py
+++ scripts/deploy_cloudformation.py
@@ -2,15 +2,26 @@
 # Upload a cloudformation template to S3, then run a stack update
 from __future__ import print_function
 from botocore.exceptions import ClientError
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
 from datetime import datetime
+from subprocess import CalledProcessError
 import __builtin__
 import argparse
 import boto3
 import os
+import re
 import sys
 import threading
 import time
+
+subservice_stack_mapping = {
+    'preprocessing': {
+        'compute': 'ComputeCluster',
+        'ingest': 'IngestStack',
+        'monitoring': 'MonitoringCluster',
+        'pipeline': 'PipelineCluster',
+    }
+}
 
 
 class Spinner:
@@ -45,22 +56,44 @@ class Spinner:
         time.sleep(self.delay)
 
 
-def get_boto3_resource(resource):
-    return boto3.resource(
-        resource,
-        region_name=args.region,
-    )
+def get_stack_name():
+    cloudformation_client = boto3.client('cloudformation', region_name=args.region)
+    res = cloudformation_client.get_paginator('list_stacks').paginate(PaginationConfig={'MaxItems': 99999})
+    stacks = [s for page in res for s in page['StackSummaries']]
+
+    if args.subservice == 'environment':
+        stack_names = [s['StackName'] for s in stacks
+                       if s['StackStatus'] != 'DELETE_COMPLETE'
+                       and s['StackName'] == '{}-{}'.format(args.service, args.environment)]
+    else:
+        stack_prefix = '{}-{}-{}'.format(
+            args.service,
+            args.environment,
+            subservice_stack_mapping[args.service][args.subservice]
+        )
+        stack_names = [s['StackName'] for s in stacks
+                       if s['StackStatus'] != 'DELETE_COMPLETE'
+                       and s['StackName'].startswith(stack_prefix)]
+
+    if len(stack_names):
+        return stack_names[0]
+    else:
+        print('Could not find stack to update', colour=Fore.RED)
+        exit(1)
 
 
 def update_cf_stack(stack, s3_path):
-    print('Updating stack {} using template {}'.format(stack.stack_name, s3_path))
+    print('Updating stack {} using template s3://{}/{}'.format(stack.stack_name, s3_bucket_name, s3_path))
+    drop_params = args.drop_params.split(',')
     stack.update(
         TemplateURL='https://s3.amazonaws.com/{bucket}/{template}'.format(
             region=args.region,
-            bucket=s3_bucket,
+            bucket=s3_bucket_name,
             template=s3_path,
         ),
-        Parameters=[{'ParameterKey': p['ParameterKey'], 'UsePreviousValue': True} for p in stack.parameters or {}],
+        Parameters=[{'ParameterKey': p['ParameterKey'], 'UsePreviousValue': True}
+                    for p in stack.parameters or {}
+                    if p['ParameterKey'] not in drop_params],
         Capabilities=['CAPABILITY_NAMED_IAM'],
     )
 
@@ -109,6 +142,25 @@ def await_stack_update(stack):
             continue
 
 
+def update_git_branch(branch_name):
+    try:
+        git_repo_name = {
+            'alerts': 'Alerts',
+            'hardware': 'Hardware',
+            'infrastructure': 'Infrastructure',
+            'preprocessing': 'PreProcessing',
+            'statsapi': 'StatsAPI',
+            'users': 'Users',
+        }[args.service]
+        git_dir = os.path.realpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../{}'.format(git_repo_name)))
+
+        os.system("git -C {} update-ref refs/heads/{} {}".format(git_dir, branch_name, args.version))
+        os.system("git -C {} push origin {} --force".format(git_dir, branch_name))
+    except CalledProcessError as e:
+        print(e.output, colour=Fore.RED)
+        raise
+
+
 def print(*args, **kwargs):
     if 'colour' in kwargs:
         __builtin__.print(kwargs['colour'], end="")
@@ -125,29 +177,50 @@ def print(*args, **kwargs):
 
 
 def main():
-    templates = map_templates(args.service, args.environment, args.subservice)
-    s3_resource = get_boto3_resource('s3')
-    for source, dest in templates:
-        s3_resource.Bucket(s3_bucket).put_object(Key=dest, Body=open(source, 'rb'))
-        print('Uploaded template from {} to {}'.format(source, dest), colour=Fore.GREEN)
+    templates = map_templates(args.service, args.environment, args.subservice, args.version)
 
-    if not args.noupdate and args.stack:
-        cf_resource = get_boto3_resource('cloudformation')
-        stack = cf_resource.Stack(args.stack)
+    s3_bucket = boto3.resource('s3').Bucket(s3_bucket_name)
+    if args.version == '0' * 40:
+        if args.environment != 'dev':
+            print('Working copy can only be deployed to dev', colour=Fore.RED)
+            exit(1)
+
+        for source, dest in templates:
+            s3_bucket.put_object(Key=dest, Body=open(source, 'rb'))
+            print('Uploaded template from {} to s3://{}/{}'.format(source, s3_bucket.name, dest), colour=Fore.GREEN)
+
+    else:
+        # Check that the CF templates have actually been uploaded
+        s3_bucket.Object(templates[0][1]).wait_until_exists()
+        print('Template exists')
+
+    if not args.noupdate:
+        stack = boto3.resource('cloudformation', region_name=args.region).Stack(get_stack_name())
 
         try:
             update_cf_stack(stack, templates[0][1])
         except ClientError as e:
             if 'No updates are to be performed' in str(e):
                 print('No updates are to be performed', colour=Fore.YELLOW)
-                exit(0)
             else:
                 print(e, colour=Fore.RED)
                 exit(1)
 
-        exit(await_stack_update(stack))
+        res = await_stack_update(stack)
+        if res == 0 and args.subservice == 'environment':
+            # Update git reference
+            # TODO
+            print('Updating git reference')
+            update_git_branch('{}-{}'.format(args.environment, args.region))
+            if args.service == 'preprocessing':
+                # We also implicitly deployed a new version of the application here
+                update_git_branch('{}-{}-app'.format(args.environment, args.region))
+            pass
 
-def map_templates(service, environment, subservice):
+        exit(res)
+
+
+def map_templates(service, environment, subservice, version):
     if subservice in ['vpc', 'apigateway']:
         # These subservices, in any service, are drawn from the infrastructure repo
         return [(
@@ -165,7 +238,7 @@ def map_templates(service, environment, subservice):
         }
         valid_subservices = {
             'alerts': ['pipeline'],
-            'infrastructure': [],
+            'infrastructure': ['timeserver', 'gitsync', 'lambci'],
             'hardware': [],
             'preprocessing': ['compute', 'ingest', 'monitoring', 'pipeline'],
             'statsapi': [],
@@ -174,12 +247,20 @@ def map_templates(service, environment, subservice):
         if service in valid_subservices:
             if subservice in valid_subservices[service] or subservice == 'environment':
                 templates = [(
-                    '{basepath}/{service}-{subservice}.yaml'.format(basepath=base_paths[service], service=service, subservice=subservice),
-                    'cloudformation/{service}-{environment}/{service}-{subservice}.yaml'.format(service=service, environment=environment, subservice=subservice)
+                    '{basepath}/{service}-{subservice}.yaml'.format(
+                        basepath=base_paths[service],
+                        service=service,
+                        subservice=subservice
+                    ),
+                    'cloudformation/{service}/{version}/{service}-{subservice}.yaml'.format(
+                        service=service,
+                        version=version,
+                        subservice=subservice
+                    )
                 )]
                 if subservice == 'environment' and service != 'infrastructure':
                     # For non-infrastructure services, updating the environment means updating all the child stacks
-                    templates += [t for s in valid_subservices[service] for t in map_templates(service, environment, s)]
+                    templates += [t for s in valid_subservices[service] for t in map_templates(service, environment, s, version)]
                 return templates
             else:
                 print('Invalid subservice for service', colour=Fore.RED)
@@ -190,10 +271,16 @@ def map_templates(service, environment, subservice):
 
 
 if __name__ == '__main__':
+    def git_commit(x):
+        if not re.match('^[0-9a-f]{40}$', x):
+            raise argparse.ArgumentTypeError('Version number must be a 40-hex-digit git hash')
+        return x
+
     parser = argparse.ArgumentParser(description='Upload a template to S3, and maybe update a CF stack using it')
-    parser.add_argument('region',
+    parser.add_argument('--region',
                         type=str,
                         choices=['us-east-1', 'us-west-2'],
+                        default='us-west-2',
                         help='AWS Region')
     parser.add_argument('service',
                         type=str,
@@ -213,18 +300,19 @@ if __name__ == '__main__':
     parser.add_argument('subservice',
                         type=str,
                         help='Service')
-    parser.add_argument('stack',
-                        type=str,
-                        nargs='?',
-                        default='',
-                        help='the name of a CF stack to update')
+    parser.add_argument('version',
+                        type=git_commit,
+                        help='the version to deploy')
     parser.add_argument('--no-update',
                         action='store_true',
                         dest='noupdate',
                         help='Skip updating CF stack')
+    parser.add_argument('--drop-params',
+                        dest='drop_params',
+                        default='',
+                        help='Parameters to drop from the template, comma-delimited')
 
     args = parser.parse_args()
 
-    s3_bucket = 'biometrix-infrastructure-{}'.format(args.region)
-    s3_base_path = 'cloudformation/{}-{}'.format(args.service, args.environment)
+    s3_bucket_name = 'biometrix-infrastructure-{}'.format(args.region)
     main()
