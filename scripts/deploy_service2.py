3c450fd27e6768192e5e5d99427553768ceffc89
Stephen Poole <stephen@melon.software>
2018-08-10 17:27:56 +0000

    Adding local/remote consistency check for branch deploys

--- scripts/deploy_service2.py
+++ scripts/deploy_service2.py
@@ -22,23 +22,9 @@ try:
 except NameError:
     pass
 
-# subservice_stack_mapping = {
-#     'infrastructure': {
-#         'security': 'infrastructure-security',
-#     },
-#     'plans': {
-#         'monitoring': 'plans-{environment}-MonitoringStack',
-#     },
-#     'preprocessing': {
-#         'compute': 'preprocessing-{environment}-ComputeCluster',
-#         'ingest': 'preprocessing-{environment}-IngestStack',
-#         'monitoring': 'preprocessing-{environment}-MonitoringCluster',
-#         'pipeline': 'preprocessing-{environment}-PipelineCluster',
-#     },
-#     'time': {
-#         'fargateecs': 'time-{environment}-FargateStack',
-#     }
-# }
+
+class ApplicationException(Exception):
+    pass
 
 lambci_builds_table = boto3.resource('dynamodb', region_name='us-east-1').Table('infrastructure-lambci-builds')
 
@@ -229,7 +215,7 @@ class Repository(object):
             FilterExpression=Attr('commit').eq(version)
         )['Items']
         if len(builds) == 0:
-            raise Exception(f'No build has been started for version {version}.  Have you pushed your changes?')
+            raise ApplicationException(f'No build has been started for version {version}.  Have you pushed your changes?')
         elif len(builds) > 1:
             cprint(f'Multiple builds found for version {version}, using most recent', colour=Fore.YELLOW)
             return max([b['buildNum'] for b in builds])
@@ -246,7 +232,7 @@ class Repository(object):
         """
         builds = lambci_builds_table.query(KeyConditionExpression=Key('project').eq(self.lambci_project_name) & Key('buildNum').eq(build_number))['Items']
         if len(builds) == 0:
-            raise Exception('Unrecognised build number')
+            raise ApplicationException('Unrecognised build number')
         else:
             return builds[0]['status']
 
@@ -300,15 +286,40 @@ class Repository(object):
                 self._execute_git_command(f'git branch --contains {version}')
                 return version, 'commit'
             except CalledProcessError:
-                raise Exception(f'Commit {version} does not exist')
+                raise ApplicationException(f'Commit {version} does not exist')
         else:
             try:
                 # Parse the value as a branch name and get the associated git commit hash
-                x2 = subprocess.check_output(f'git rev-parse {version}', cwd=self._git_dir, shell=True).decode('utf-8').strip()
+                x2 = self._execute_git_command(f'git rev-parse {version}')
                 cprint(f"Branch '{version}' has commit hash {x2}", colour=Fore.GREEN)
                 return x2, 'branch'
             except CalledProcessError:
-                raise Exception('Version must be a 40-hex-digit git hash or valid branch name')
+                raise ApplicationException('Version must be a 40-hex-digit git hash or valid branch name')
+
+    def compare_remote_status(self, branch_name):
+        """
+        Check whether the local version of a branch is behind, level with or ahead of the remote
+        :param str branch_name:
+        :return: int
+        """
+        # Update remotes first
+        self._execute_git_command('git remote update')
+
+        local_ref = self._execute_git_command(f'git rev-parse {branch_name}')
+
+        remote_ref = self._execute_git_command(f'git rev-parse origin/{branch_name}')
+
+        # The first common ancestor of the local and remote branches
+        parent_ref = self._execute_git_command(f'git merge-base {branch_name} origin/{branch_name}')
+
+        if local_ref == remote_ref:
+            return 0  # Branches are equal
+        elif local_ref == parent_ref:
+            return -1  # The remote is ahead of local
+        elif remote_ref == parent_ref:
+            return 1  # The local ref is ahead of remote
+        else:
+            raise ApplicationException(f'Branch {branch_name} has diverged from origin/{branch_name}')
 
     def update_git_branch(self, branch_name, version):
         """
@@ -334,10 +345,10 @@ class Repository(object):
             return os.path.realpath(
                 os.path.join(os.path.dirname(os.path.abspath(__file__)), f'../../{git_repo_name}'))
         except KeyError:
-            raise Exception(f'No Git repository configured for service {self.service}')
+            raise ApplicationException(f'No Git repository configured for service {self.service}')
 
     def _execute_git_command(self, command):
-        return subprocess.check_call(command, cwd=self._git_dir, shell=True)
+        return subprocess.check_output(command, cwd=self._git_dir, shell=True).decode('utf-8').strip()
 
     @property
     def lambci_project_name(self):
@@ -360,7 +371,7 @@ def map_config(old_config):
     # Rename parameters
     for old_key, new_key in param_renames.items():
         if old_key not in new_config:
-            raise Exception(f'{old_key} not found in existing config, cannot rename')
+            raise ApplicationException(f'{old_key} not found in existing config, cannot rename')
 
         if new_key == '':
             cprint(f'Removing parameter {old_key}')
@@ -443,15 +454,22 @@ def main():
     # Do the pre-upload of templates to the 00000000 version
     if ref_type == 'head':
         if args.environment != 'dev':
-            raise Exception('Working copy can only be deployed to dev')
+            raise ApplicationException('Working copy can only be deployed to dev')
         service.repository.upload_working_copy()
+
+    elif ref_type == 'branch':
+        comparison = service.repository.compare_remote_status(args.version)
+        if comparison == -1:
+            confirm(f'Branch {args.version} is behind origin/{args.version}.  Are you sure you want to deploy an earlier version?')
+        elif comparison == 1:
+            raise ApplicationException(f'Branch {args.version} is ahead of origin/{args.version}.  You need to push your changes')
 
     # Check that the build we're about to deploy has actually been completed
     service.repository.await_build_completion(service_version)
 
     if args.subservice:
         if ref_type != 'head':
-            raise Exception('Can only update subservice with working copy')
+            raise ApplicationException('Can only update subservice with working copy')
         else:
             # TODO
             raise NotImplementedError()
@@ -538,6 +556,9 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         cprint('Exiting', colour=Fore.YELLOW)
+        exit(1)
+    except ApplicationException as ex:
+        cprint(str(ex), colour=Fore.RED)
         exit(1)
     except Exception as ex:
         cprint(str(ex), colour=Fore.RED)
