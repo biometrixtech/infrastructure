5a7aa7e64056d671f0a0cbed51d4ac7932880d7f
Stephen Poole <stephen@melon.software>
2018-08-22 16:41:35 +0000

    Refactoring Lambda/Apigateway updates

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -15,14 +15,17 @@ from components.repository import Repository
 from components.ui import confirm, cprint, Spinner
 
 
+apigateway_client = None
+
+
 class Environment(object):
     def __init__(self, region, environment):
         self.region = region
         self.name = environment
 
-        self.stack = boto3.resource('cloudformation', region_name=self.region).Stack(self._get_stack_name())
-        self._config = {p['ParameterKey']: p['ParameterValue'] for p in self.stack.parameters or []}
+        self._config = None
         self._stack_template_url = None
+        self._stack = None
 
         self._repository = Repository('infrastructure')
 
@@ -32,7 +35,15 @@ class Environment(object):
 
     @property
     def config(self):
+        if self._config is None:
+            self._config = {p['ParameterKey']: p['ParameterValue'] for p in self.stack.parameters or []}
         return self._config
+
+    @property
+    def stack(self):
+        if self._stack is None:
+            self._stack = boto3.resource('cloudformation', region_name=self.region).Stack(self._get_stack_name())
+        return self._stack
 
     def update_environment_version(self, version):
         if version is not None:
@@ -138,8 +149,14 @@ class Environment(object):
     def get_service_repository(self, service):
         return self._get_service(service).repository
 
-    def update_lambda_functions(self, service, service_version):
-        self._get_service(service).update_lambda_functions(service_version)
+    def update_lambda_functions(self, service, ref, tag=None):
+        service = self._get_service(service)
+        service.update_lambda_functions(ref, tag is not None)
+        if tag is not None:
+            service.create_lambda_aliases(tag)
+
+    def create_apigateway_stages(self, service, tag):
+        self._get_service(service).create_apigateway_stages(tag=tag)
 
     def _get_stack_name(self):
         return f'infrastructure-{self.name}'
@@ -150,8 +167,8 @@ class Environment(object):
 
 class LegacyEnvironment(Environment):
     def __init__(self, region, environment, service):
-        self._service = super()._get_service(service)
         super().__init__(region, environment)
+        self._service = super()._get_service(service)
         self._repository = self._service.repository
 
     def _get_service(self, service):
@@ -178,19 +195,163 @@ class Service(object):
 
         self._repository = Repository(self.name)
 
+        config = self.repository.get_config()
+        self._lambda_functions = [LambdaFunction(self, la['name'], la['s3_filename']) for la in config['lambdas']]
+        self._api_gateways = [ApiGateway(self, ag['name'], self._get_lambda_function(ag['lambda_function_name'])) for ag in config.get('apigateways', [])]
+
     @property
     def repository(self):
         return self._repository
 
-    def update_lambda_functions(self, version):
-        config = self.repository.get_config()
+    def update_lambda_functions(self, ref, publish_tags=False):
+        for lambda_function in self._lambda_functions:
+            lambda_function.update_code(ref, publish_tags)
 
-        lambda_client = boto3.client('lambda', region_name=self.environment.region)
-        for lambda_bundle in config['lambdas']:
-            lambda_name = lambda_bundle['name'].format(ENVIRONMENT=self.environment.name)
-            s3_filepath = 'lambdas/{}/{}/{}'.format(self.name, version, lambda_bundle['s3_filename'])
-            cprint(f'Updating Lambda {lambda_name} with bundle s3://{s3_filepath}')
-            lambda_client.update_function_code(FunctionName=lambda_name, S3Bucket=s3_bucket_name, S3Key=s3_filepath)
+    def create_lambda_aliases(self, tag):
+        for lambda_function in self._lambda_functions:
+            lambda_function.create_alias(tag)
+
+    def create_apigateway_stages(self, tag):
+        for apigateway in self._api_gateways:
+            apigateway.create_stage(tag)
+
+    def _get_lambda_function(self, name):
+        name = name.format(ENVIRONMENT=self.environment.name)
+        for lambda_function in self._lambda_functions:
+            if lambda_function.name == name:
+                return lambda_function
+        raise Exception(f'Could not find lambda function {name}')
+
+
+class LambdaFunction:
+    def __init__(self, service, name, s3_filepath):
+        self._service = service
+        self._name = name.format(ENVIRONMENT=service.environment.name)
+        self._s3_filepath = s3_filepath
+
+    @property
+    def name(self):
+        return self._name
+
+    def get_latest_version(self):
+        return self._get_all_versions()[-1][1]
+
+    def create_alias(self, semantic_version: VersionInfo, lambda_version=None):
+        alias_name = self.semantic_version_to_alias_name(semantic_version)
+
+        if lambda_version is None:
+            lambda_version = self.get_latest_version()
+
+        cprint(f'Tagging version {self.name}:{lambda_version} as alias {alias_name}', colour=Fore.CYAN)
+        lambda_client.create_alias(
+            FunctionName=self._name,
+            Name=alias_name,
+            FunctionVersion=lambda_version
+        )
+
+    def add_apigateway_permission(self, semantic_version: VersionInfo, apigateway):
+        alias_name = self.semantic_version_to_alias_name(semantic_version)
+        lambda_client.add_permission(
+            FunctionName=self._name,
+            StatementId=alias_name,
+            Action='lambda:InvokeFunction',
+            Principal='apigateway.amazonaws.com',
+            SourceArn=f'arn:aws:execute-api:{args.region}:887689817172:{apigateway.id}/*',
+            Qualifier=alias_name
+        )
+
+    def update_alias(self, semantic_version: VersionInfo, target_alias):
+        """
+        Update an existing lambda alias to point to another alias
+        :param target_alias:
+        :param semantic_version:
+        :return:
+        """
+        target_version = self._get_version_of_alias(self.semantic_version_to_alias_name(target_alias))
+
+        lambda_client.update_alias(
+            FunctionName=self._name,
+            Name=self.semantic_version_to_alias_name(semantic_version),
+            FunctionVersion=target_version
+        )
+
+    def update_code(self, ref, publish_version=False):
+        s3_filepath = 'lambdas/{}/{}/{}'.format(self._service.name, ref, self._s3_filepath)
+        cprint(f'Updating Lambda {self._name} with bundle s3://{s3_filepath}', colour=Fore.CYAN)
+        lambda_client.update_function_code(
+            FunctionName=self._name,
+            S3Bucket=s3_bucket_name,
+            S3Key=s3_filepath,
+            Publish=publish_version
+        )
+
+    def _get_version_of_alias(self, alias_name):
+        return lambda_client.get_function(FunctionName=self._name, Qualifier=alias_name)['Version']
+
+    def _get_all_versions(self, next_marker=None):
+        if next_marker is not None:
+            res = lambda_client.list_versions_by_function(FunctionName=self._name, Marker=next_marker)
+        else:
+            res = lambda_client.list_versions_by_function(FunctionName=self._name)
+        ret = [(v['LastModified'], v['Version']) for v in res['Versions'] if v['Version'] != '$LATEST']
+        if 'NextMarker' in res:
+            ret += self._get_all_versions(res['NextMarker'])
+        return sorted(ret)
+
+    @staticmethod
+    def semantic_version_to_alias_name(semantic_version: VersionInfo):
+        return str(semantic_version).replace('.', '_')
+
+
+class ApiGateway:
+    _id = None
+
+    def __init__(self, service, name, lambda_function: LambdaFunction):
+        self._service = service
+        self._name = name.format(ENVIRONMENT=service.environment.name)
+        self._lambda_function = lambda_function
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def id(self):
+        if self._id is None:
+            for api in self._get_all_rest_apis():
+                if api['name'] == self._name:
+                    self._id = api['id']
+                    break
+            else:
+                raise Exception(f'API Gateway {self.name} was not found')
+        return self._id
+
+    @staticmethod
+    def _get_all_rest_apis():
+        all_apis = apigateway_client.get_rest_apis()
+        # TODO paging
+        return all_apis['items']
+
+    def get_latest_deployment_id(self):
+        all_deployments = apigateway_client.get_deployments(restApiId=self.id)['items']
+        deployment_id = sorted(all_deployments, key=lambda x: x['createdDate'])[-1]['id']
+        return deployment_id
+
+    def create_stage(self, semantic_version: VersionInfo):
+        deployment_id = self.get_latest_deployment_id()
+        stage_name = self.semantic_version_to_stage_name(semantic_version)
+        cprint(f'Creating API Gateway stage {self.id}/{deployment_id}/{stage_name} for {self.name}', colour=Fore.CYAN)
+        apigateway_client.create_stage(
+            restApiId=self.id,
+            deploymentId=deployment_id,
+            stageName=stage_name,
+            variables={'LambdaAlias': LambdaFunction.semantic_version_to_alias_name(semantic_version)}
+        )
+        self._lambda_function.add_apigateway_permission(semantic_version, self)
+
+    @staticmethod
+    def semantic_version_to_stage_name(semantic_version: VersionInfo):
+        return str(semantic_version).replace('.', '_')
 
 
 def map_config(old_config):
@@ -247,7 +408,6 @@ def ucfirst(s: str) -> str:
 
 
 def main():
-    boto3.setup_default_session(profile_name=args.profile_name)
 
     if args.environment in ['dev', 'production']:
         environment = LegacyEnvironment(args.region, args.environment, args.service)
@@ -285,7 +445,7 @@ def main():
         else:
             raise ApplicationException('Deployments to environments above dev must be tagged')
     else:
-        version = VersionInfo.parse(args.tag or args.ref)
+        version = VersionInfo.parse(args.tag)
         validate_semver_tag(version, service_repository.get_semver_tags())
 
     #             | head                  | commit    | branch                  | tag
@@ -293,7 +453,7 @@ def main():
     #  dev        | upload_working_copy() | Ok        | compare_remote_status() | Ok
     #  test       | X                     | confirm() | compare_remote_status() | confirm()
     #  <other>    | X                     | confirm() | compare_remote_status() | confirm()
-    #  production | X                     | confirm() | X                       | confirm()
+    #  production | X                     | confirm() | compare_remote_status() | confirm()
 
     # Do the pre-upload of templates to the 00000000 version
     if ref_type == 'head':
@@ -303,22 +463,19 @@ def main():
             raise ApplicationException('Working copy can only be deployed to dev')
 
     if ref_type == 'branch':
-        if args.environment == 'production':
-            raise ApplicationException(f'Only tagged versions can be deployed to production. Run with [deploy.py {args.environment} {args.service} --ref {args.tag}')
-        else:
-            comparison = service_repository.compare_remote_status(args.ref)
-            if comparison == -1:
-                if not confirm(f'Branch {args.version} is behind origin/{args.ref}.  Are you sure you want to deploy an earlier version?'):
-                    exit(0)
-            elif comparison == 1:
-                raise ApplicationException(f'Branch {args.ref} is ahead of origin/{args.ref}.  You need to push your changes')
+        comparison = service_repository.compare_remote_status(args.ref)
+        if comparison == -1:
+            if not confirm(f'Branch {args.version} is behind origin/{args.ref}.  Are you sure you want to deploy an earlier version?'):
+                exit(0)
+        elif comparison == 1:
+            raise ApplicationException(f'Branch {args.ref} is ahead of origin/{args.ref}.  You need to push your changes')
 
     if version is not None:
         cprint(f"Going to tag commit {service_version[0:16]} (from {ref_type} {args.ref}) as {version} and deploy to {args.environment}", colour=Fore.YELLOW)
     else:
         cprint(f"Going to deploy commit {service_version[0:16]} (from {ref_type} {args.ref}) to {args.environment}", colour=Fore.YELLOW)
 
-    if args.environment != 'dev' or version is not None:
+    if not args.yes and (args.environment != 'dev' or version is not None):
         if not confirm('Is this what you wanted? '):
             exit(0)
 
@@ -356,8 +513,19 @@ def main():
         cprint('Updating git reference')
         service_repository.update_git_branch(f'{args.environment}-{args.region}', service_version)
 
-        # Explicitly deploy lambda functions to make sure they update
-        environment.update_lambda_functions(args.service, service_version)
+    if version is not None:
+        # Create versions and aliases of lambda functions and set up a stage in API Gateway
+        environment.update_lambda_functions(args.service, ref=service_version, tag=version)
+        environment.create_apigateway_stages(args.service, version)
+
+        if version.prerelease is None:
+            # Deploying a 'proper' build, so 'slide' the partially-pinned aliases up to the new version
+            # TODO
+            pass
+
+    else:
+        # Explicitly deploy lambda functions anyway to make sure the $LATEST version is up to date
+        environment.update_lambda_functions(args.service, ref=service_version)
 
 
 if __name__ == '__main__':
@@ -408,10 +576,17 @@ if __name__ == '__main__':
                         dest='profile_name',
                         default='default',
                         help='boto3 profile to use')
+    parser.add_argument('-y',
+                        dest='yes',
+                        action='store_true',
+                        help='Skip confirmations')
 
     args = parser.parse_args()
 
     s3_bucket_name = 'biometrix-infrastructure-{}'.format(args.region)
+    boto3.setup_default_session(profile_name=args.profile_name, region_name=args.region)
+    apigateway_client = boto3.client('apigateway')
+    lambda_client = boto3.client('lambda')
 
     try:
         main()
